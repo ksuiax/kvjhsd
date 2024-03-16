7e9c0bda18b8b263f8dd354a54eb36a1023df243
ksuiax <162794990+ksuiax@users.noreply.github.com>
2024-03-16 12:51:05 +0500

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -1,1 +1,7 @@
 # напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import(
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
